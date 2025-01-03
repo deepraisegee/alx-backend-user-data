bb7d45e72ba51ed356765bb5e09f7e190c4c7111
Dayo P. Adesanmi <deepraisegee@gmail.com>
2025-01-03 07:19:40 +0100

    # 0x03-user_authentication_service

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,41 @@
+"""DB module
+"""
+
+import bcrypt
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class"""
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance"""
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object"""
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """create user and save to database"""
+        user = User()
+        user.email = email
+        user.hashed_password = bcrypt.hashpw(hashed_password.encode(), bcrypt.gensalt())
+        # save ro db
+        self._session.add(user)
+        self._session.commit()
+
+        return user
