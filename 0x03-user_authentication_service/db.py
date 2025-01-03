25a1fb74e2ee11a68129b446005c30194313af6a
Dayo P. Adesanmi <deepraisegee@gmail.com>
2025-01-03 07:22:07 +0100

    # 0x03-user_authentication_service

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,6 @@
 import bcrypt
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
